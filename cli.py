446616b3a6cf0d296a3e0af1c4c1c640ae2aca23
crystalreef22 <119718492+crystalreef22@users.noreply.github.com>
2024-03-29 15:43:29 -0400

    Tried to start qt creator

---
+++ cli.py
@@ -0,0 +1,27 @@
+#!/Users/etn/Programming/py-venv/bin/python3
+import signal, sys
+import time
+import quoteGenerator as qg
+import txtEncoder as te
+
+
+qg.init() #Ideally, I should make this a class
+
+aristocrat = te.AristocratEncoder()
+kg = te.keyWordGenerator("./commonWordList.txt")
+
+while True:
+    q = qg.getNextQuote()
+    plaintext = q['q']
+    author = q['a']
+    del q
+
+    key = kg.next()
+    ciphertext = aristocrat.encrypt(plaintext)
+
+    print(ciphertext)
+    print("\t--", author)
+    input("\n")
+    print(plaintext)
+    print("Key:", kg.current)
+    print('\n'*4)
--- main.py
+++ main.py
@@ -1,27 +1,16 @@
-#!/Users/etn/Programming/py-venv/bin/python3
-import signal, sys
-import time
-import quoteGenerator as qg
-import txtEncoder as te
+# This Python file uses the following encoding: utf-8
+import sys
+from pathlib import Path
+
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtQml import QQmlApplicationEngine
 
 
-qg.init() #Ideally, I should make this a class
-
-aristocrat = te.AristocratEncoder()
-kg = te.keyWordGenerator("./commonWordList.txt")
-
-while True:
-    q = qg.getNextQuote()
-    plaintext = q['q']
-    author = q['a']
-    del q
-
-    key = kg.next()
-    ciphertext = aristocrat.encrypt(plaintext)
-
-    print(ciphertext)
-    print("\t--", author)
-    input("\n")
-    print(plaintext)
-    print("Key:", kg.current)
-    print('\n'*4)
+if __name__ == "__main__":
+    app = QGuiApplication(sys.argv)
+    engine = QQmlApplicationEngine()
+    qml_file = Path(__file__).resolve().parent / "main.qml"
+    engine.load(qml_file)
+    if not engine.rootObjects():
+        sys.exit(-1)
+    sys.exit(app.exec())
